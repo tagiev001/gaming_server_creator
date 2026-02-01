9a97a82ff977149012a1b61c4ea772d9a37c8b24
user <ruslantagiev21@gmail.com>
2026-02-01 23:09:44 +0300

    some basics

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import docker
 
 
-def build_image(tag: str, path: str):
+
+
+def build_image(tag: str, dockerfile: str,):
     client = None
     try:
         client = docker.from_env()
-        image, logs = client.images.build(path=path, tag=tag, rm=True)
+        image, logs = client.images.build(dockerfile=dockerfile, tag=tag, rm=True)
         return image.tags[0]
 
     except docker.errors.BuildError as err:
@@ -22,28 +24,31 @@ def build_image(tag: str, path: str):
             client.close()
         
 
-def locate_dockerfiles(search_path: str)
+def locate_dockerfiles(search_path: str):
     pass
 
 
-def run_container()
+def run_gaming_server(game, ports, IPv4, ram, rom, game_files_path):
     pass
 
 
-def add_info_about_container()
+def add_info_about_container():
     pass
 
 
-def remove_info_about_container()
+def remove_info_about_container():
     pass
 
 
-def stop_container()
+def stop_container():
     pass
 
 
-def make_backup()
+def make_backup():
     pass
 
 
-print(build_image("test:v1.1", "."))
+
+SUPPORTED_GAMES = ['Minecraft', 'Terraria', 'Project Zomboid', 'Factorio']
+
+print(build_image(SUPPORTED_GAMES[0], "mc/Dockerfilemc"))
